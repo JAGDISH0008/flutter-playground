359ddbd6e4db3a6b392ae7efbe7a9cbdb92d2505
JAGDISH0008 <jagadeesh26597@gmail.com>
2020-05-21 06:41:52 +0530

    function call backs

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:first_app/answer.dart';
+import 'package:first_app/question.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
-class MyApp extends StatefulWidget{
+
+class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -11,38 +14,44 @@ class MyApp extends StatefulWidget{
   }
 }
 
-class MyAppState extends State<MyApp>{
-  var questions=[
+class MyAppState extends State<MyApp> {
+  var questions = [
     'Question 1',
     'Question 2',
     'Question 3',
     'Question 4',
     'Question 5',
   ];
-  var i=0;
-  click(){
+  var i = 0;
+  click() {
     setState(() {
-      i+=1;
+      i += 1;
     });
     print(i);
   }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
-        appBar: AppBar(title: Text('My App'),),
-        body: Column(
-          children: <Widget>[
-            Text(questions[i],
-            textAlign: TextAlign.center),
-            RaisedButton(child: Text('Press Me'),onPressed: click,),
-          ],
-          
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Question(questions[i]),
+          // RaisedButton(
+          //   child: Text('Press Me'),
+          //   onPressed: click,
+          // textColor: Colors.indigoAccent,
+          // ),
+          Answer(click),
+          Answer(click),
+          Answer(click),
+          Answer(click),
 
-        ),
-
-      )
-    );
+        ],
+      ),
+    ));
   }
-
 }
